bd4ebf19948680b2836aaab1629329454eda68e9
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2022-01-04 14:41:59 +0000

    Sync

--- Assets/FireEffects.cs
+++ Assets/FireEffects.cs
@@ -2,16 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireEffects : MonoBehaviour
+public class GunfireEffect : MonoBehaviour
 {
-    
-    void Start()
+    public GameObject launchEffect;
+    public GameObject startPoint;
+
+    private GameObject launchObject;
+    private List<UnityEngine.XR.InputDevice> devices = new List<UnityEngine.XR.InputDevice>();
+
+    private void Start()
     {
-        
+        UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.HeldInHand, devices);
     }
 
-    void Update()
+    public void Fire()
     {
-        
+        //Launch particle effect
+        launchObject = Instantiate(launchEffect, startPoint.transform.position, startPoint.transform.rotation);
+
+        foreach (var device in devices)
+        {
+            if (device.TryGetHapticCapabilities(out UnityEngine.XR.HapticCapabilities capabilities)
+                && capabilities.supportsImpulse)
+            {
+                uint channel = 0;
+                float amplitude = 1.0f;
+                float duration = 1.0f;
+                device.SendHapticImpulse(channel, amplitude, duration);
+            }
+        }
+
+        //Cleanup for efficiency
+        Destroy(launchObject, 2);
     }
 }
